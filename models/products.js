bf3aa241e899851d815d428eff395b5a6bc0bfe4
mintmintra <mintracss019@gmail.com>
2021-12-03 01:52:52 +0700

    first commit

---
+++ models/products.js
@@ -0,0 +1,27 @@
+//ใช้งาน Mongoose
+const mongoose = require('mongoose')
+
+//เชื่อมไปยัง MondoDB
+const dbUrl = 'mongodb://localhost:27017/productDB'
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}).catch(err => console.log(err))
+
+//ออกแบบ Schema
+let productSchema = mongoose.Schema({
+    name: String,
+    price: Number,
+    image: String,
+    description:String
+})
+//สร้างโมเดล
+let Product = mongoose.model("products",productSchema)
+
+//ส่งออกโมเดล
+module.exports = Product
+
+//ออกแบบฟังก์ชั่นสำหรับบันทึกข้อมูล
+module.exports.saveProduct = function (model, data) {
+    model.save(data)
+}
